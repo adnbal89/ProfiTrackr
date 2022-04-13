efb6f0a743b92cb39d398e7417e1d7f535bbc6e6
adnbal89 <adnbal89@gmail.com>
2022-04-13 17:12:24 +0300

    DI implemented

--- app/src/main/java/com/fxingsign/profitrackr/di/AppModule.kt
+++ app/src/main/java/com/fxingsign/profitrackr/di/AppModule.kt
@@ -1,12 +1,44 @@
 package com.fxingsign.profitrackr.di
 
+import com.fxingsign.profitrackr.data.remote.StockQuoteApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }).build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(StockQuoteApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideStockQuoteApi(retrofit: Retrofit): StockQuoteApi {
+        return retrofit.create(StockQuoteApi::class.java)
+    }
 
 }
