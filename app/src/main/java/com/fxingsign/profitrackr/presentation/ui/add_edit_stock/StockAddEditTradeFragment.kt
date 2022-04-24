d0a665a46655ec99bf1bab40bd58b7f705f12cf8
Adnan BAL <adnbal89@hotmail.com>
2022-04-24 21:07:47 +0300

    several bug fix.

--- app/src/main/java/com/fxingsign/profitrackr/presentation/ui/add_edit_stock/StockAddEditTradeFragment.kt
+++ app/src/main/java/com/fxingsign/profitrackr/presentation/ui/add_edit_stock/StockAddEditTradeFragment.kt
@@ -1,14 +1,19 @@
 package com.fxingsign.profitrackr.presentation.ui.add_edit_stock
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.fxingsign.profitrackr.R
 import com.fxingsign.profitrackr.databinding.FragmentAddEditStockTradeBinding
 import com.fxingsign.profitrackr.domain.repository.stocks.dto.StockTradeDto
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class StockAddEditTradeFragment : Fragment(R.layout.fragment_add_edit_stock_trade) {
@@ -20,24 +25,40 @@ class StockAddEditTradeFragment : Fragment(R.layout.fragment_add_edit_stock_trad
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val stocks = resources.getStringArray(R.array.stock_symbol_array)
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1, stocks
+        )
+
         val binding = FragmentAddEditStockTradeBinding.bind(view)
         binding.apply {
-
-
+            autocompleteTextViewSymbol.setAdapter(adapter)
             buttonBuy.setOnClickListener {
-                stockId = editTextSymbol.text.toString()
-                val stockTradeDto = createStockTradeDtoFromUi(binding)
-                val copyStockTradeDto = stockTradeDto.copy(symbol = stockId, tradeType = "buy")
-                viewModel.insertStockTrade(copyStockTradeDto)
-                findNavController().navigateUp()
+                if (!stocks.contains<String>(autocompleteTextViewSymbol.text.toString())) {
+                    showInvalidStockIdSnackBar()
+                    hideKeyboard(requireContext(), autocompleteTextViewSymbol)
+                } else {
+
+                    val stockTradeDto = createStockTradeDtoFromUi(binding)
+                    val copyStockTradeDto = stockTradeDto.copy(tradeType = "buy")
+                    viewModel.insertStockTrade(copyStockTradeDto)
+                    findNavController().navigateUp()
+                }
             }
 
             buttonSell.setOnClickListener {
-                stockId = editTextSymbol.text.toString()
-                val stockTradeDto = createStockTradeDtoFromUi(binding)
-                val copyStockTradeDto = stockTradeDto.copy(symbol = stockId, tradeType = "sell")
-                viewModel.insertStockTrade(copyStockTradeDto)
-                findNavController().navigateUp()
+                if (!stocks.contains<String>(autocompleteTextViewSymbol.text.toString())) {
+                    showInvalidStockIdSnackBar()
+                    hideKeyboard(requireContext(), autocompleteTextViewSymbol)
+                } else {
+
+                    val stockTradeDto = createStockTradeDtoFromUi(binding)
+                    val copyStockTradeDto = stockTradeDto.copy(tradeType = "sell")
+                    viewModel.insertStockTrade(copyStockTradeDto)
+                    findNavController().navigateUp()
+                }
             }
 
         }
@@ -45,13 +66,25 @@ class StockAddEditTradeFragment : Fragment(R.layout.fragment_add_edit_stock_trad
 
     private fun createStockTradeDtoFromUi(binding: FragmentAddEditStockTradeBinding): StockTradeDto {
         return StockTradeDto(
-            "ASELS",
+            binding.autocompleteTextViewSymbol.text.toString(),
             binding.editTextQuantity.text.toString().toInt(),
             binding.editTextPrice.text.toString().toDouble(),
-            binding.editTextPrice.text.toString(),
+            binding.datePickerDate.text.toString(),
             "buy"
         )
     }
 
+    private fun showInvalidStockIdSnackBar() {
+        Snackbar.make(
+            requireView(),
+            "Stock not found in the list.",
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    private fun hideKeyboard(context: Context, view: View) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 
 }
